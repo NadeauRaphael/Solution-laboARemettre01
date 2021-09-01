537e5447c4e0d2fb91885fb2fbc81a15167a47cf
Raphael Nadeau <raphynadeau@hotmail.com>
2021-09-01 12:18:59 -0400

    fin du devoir

---
+++ Projet-Exercice 2/Verticalcpp.cpp
@@ -0,0 +1,37 @@
+// But: Écrivez un programme qui prend comme entrée un nombre de cinq chiffres. 
+// Le programme vérifie qu'il y a bien 5 chiffres dans le nombre.
+// Puis il affiche le nombre verticalement, le premier chiffre sur la première ligne,
+// le deuxième chiffre sur la deuxième ligne, ainsi de suite
+// Auteur : Raphael Nadeau
+// Date : 01-09-2021
+
+#include <iostream>
+
+
+void main()
+{
+	int nombre;
+	int chiffre1;
+	int chiffre2;
+	int chiffre3;
+	int chiffre4;
+	int chiffre5;
+
+
+	std::cout << "entrer un nombre a 5 chiffres:";
+	std::cin >> nombre;
+	if  (nombre > 9999 && nombre < 100000 )
+	{
+		chiffre1 = nombre % 10;
+		chiffre2 = nombre / 10 % 10;
+		chiffre3 = nombre / 100 % 10;
+		chiffre4 = nombre / 1000 % 10;
+		chiffre5 = nombre / 10000 % 10;
+		std::cout << chiffre5 << std::endl << chiffre4 << std::endl << chiffre3 << std::endl << chiffre2 << std::endl << chiffre1 << std::endl;
+	}
+	else
+	{
+		std::cout << "Ce nombre ne contient pas 5 chiffres";
+	}
+
+}
